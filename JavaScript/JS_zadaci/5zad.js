f494c054fa81203b10b8a6e53f5228f888c67820
peka80 <petar.dragovic@gmail.com>
2021-03-31 01:42:07 +0200

    zadatak 5 i 6 reseni

--- JavaScript/JS_zadaci/5zad.js
+++ JavaScript/JS_zadaci/5zad.js
@@ -1,3 +1,12 @@
 document.write(
   '<b>Pomocu while petlje napisati program koji racuna zbir prvih 20 neparnih brojeva.</b><br><br>'
 );
+
+let broj = 1;
+let zbir = 0;
+
+while (broj < 40) {
+  zbir = zbir + broj;
+  broj += 2;
+}
+document.write('Zbir prvih 20 neparnih brojeva je ' + zbir + '.');
---
+++ JavaScript/JS_zadaci/6zad.js
@@ -0,0 +1,47 @@
+document.write(
+  '<b>Napraviti promenljive a i b, koje sadrže proizvoljne brojeve, i promenljivu <u><i>operacija</i></u> koja sadrzi string sa nazivom operacije. (moze da bude ‘saberi’, ‘oduzmi’, ‘pomnozi’, ‘podeli’). Ispisati rezultat operacije čiji naziv je sadržan u promenljivoj operacija sa vrednostima a i b. Npr. ako je vrednost promenljive operacija ‘saberi’, ispisati rezultat a + b (koristiti switch).</b><br><br>'
+);
+
+let a = 5;
+let b = 10;
+let operacija = 'Podeli';
+
+switch (operacija) {
+  case 'Saberi':
+    document.write(
+      'Zbir promenljive A (' + a + ') i promenljive B (' + b + ') je ' + (a + b)
+    );
+    break;
+  case 'Oduzmi':
+    document.write(
+      'Razlika promenljive A (' +
+        a +
+        ') i promenljive B (' +
+        b +
+        ') je ' +
+        (a - b)
+    );
+    break;
+  case 'Pomnozi':
+    document.write(
+      'Proizvod promenljive A (' +
+        a +
+        ') i promenljive B (' +
+        b +
+        ') je ' +
+        a * b
+    );
+    break;
+  case 'Podeli':
+    document.write(
+      'Količnik promenljive A (' +
+        a +
+        ') i promenljive B (' +
+        b +
+        ') je ' +
+        a / b
+    );
+    break;
+  default:
+    document.write('Nepoznata operacija!');
+}
